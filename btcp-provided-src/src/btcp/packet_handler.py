6c379b568cc112f83e5143084a1b2124c1efd25c
WaterstofBrug <dieksscholten@gmail.com>
2024-03-08 16:54:11 +0100

    nog een keer proberen

--- btcp-provided-src/src/btcp/packet_handler.py
+++ btcp-provided-src/src/btcp/packet_handler.py
@@ -8,7 +8,8 @@ class PacketHandler(ABC):
     def __init__(self, window_size, handler_type, ISN=0):
         self.send_base = 0                          # send base is the head of the window; ie the index of the first element in the window to be send
         self.current_SN = ISN + 1                   # starting sequence number for the protocol; +1 because we just send 2 segments as client. (3-way handshake)
-        self.ack_queue = queue.Queue()     # ack queue keeps track of the acks to be received, and in the specified order
+        self.expected_ACK_queue = queue.Queue()     # ack queue keeps track of the acks to be received, and in the specified order
+        self.seg_queue = queue.Queue()
 
 
         def send_data(self, data: bytes) -> None:       # takes a byte object, turns it into 1008 byte pieces, turns those into segments, sends them
@@ -20,24 +21,40 @@ class PacketHandler(ABC):
                 else:
                     pkt_queue.put(data)
             
-            seg_queue = self.build_seg_queue(self, list(pkt_queue))
-            self.send_segment_queue(self, seg_queue) 
+            for seg in self.build_seg_queue(self, list(pkt_queue)):
+                self.seg_queue.put(seg)
+            self.send_window_seqments(self, seg_queue) 
 
             return None
 
 
-        def rcvd_seg(self, seq_field, ack_field, ACK, payload: bytes) -> bytes: # handle incoming traffic; differentiate between a packet with the ACK set, and a data packet.  
+        def handle_rcvd_seg(self, seq_field, ack_field, ACK, payload: bytes) -> bytes: # handle incoming traffic; differentiate between a packet with the ACK set, and a data packet. 
+            """ 
+            A segment is recieved by a socket and unpacked. The payload and part of the unpacked header
+            is given as input. This is handled by the specific instance of the handler. This function returns
+            the data recieved in correct order. If a call contains not in-order data the function will return 
+            an empty bytes object and depending on the specific handler it might buffer or discard the data recieved.
+            """ 
             if ACK:
                 return self.handle_ack(self, seq_field, ack_field, payload)
             return self.handle_data(self, seq_field, ack_field, payload)
 
 
         @abstractmethod
-        def build_seg_queue(self):
+        def build_seg_queue(self, pkt_list):
+            """
+            This function constructs a queue of segments. These segments are constructed by the 
+            specific handler and based on the data. 
+            """
             pass
 
         @abstractmethod
-        def send_seq_queue(self, seg_queue):
+        def send_window_seqments(self):
+            """
+            This functions sends all segments within the window which the specific handler decides
+            to send. This function needs to be called every time the send_base is updated and when
+            the specifc handler needs to send all segments in the window.
+            """
             pass 
 
         @abstractmethod
@@ -46,24 +63,10 @@ class PacketHandler(ABC):
 
         @abstractmethod
         def handle_data(self, seq_field, ack_field, payload):
-            '''
-            
-            '''
             pass 
 
         @abstractmethod
-        def build_ack_queue(self):  
-            ''' 
-            the purpose of the ack queue is holding the acknumbers of the to be expected acks
-            if the received ack is not equal to the first element in the queue with acks, the packets are out of order.
-            Though, this function just builds the ack queue.
-            '''
+        def build_ack_queue(self):
             pass
 
-        @abstractmethod
-        def handle_ack_queue(self):
-            '''
-            updates the ack queue when elements are popped, and pushes ack numbers onto them.
-            '''
-            pass
 
