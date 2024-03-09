d83cb3fe89c0ad37411ca472aedf9c9c30716117
sevy <jui.yvesvanhaaren@gmail.com>
2024-03-09 14:31:09 +0100

    djfkd

--- btcp-provided-src/src/btcp/packet_handler.py
+++ btcp-provided-src/src/btcp/packet_handler.py
@@ -18,95 +18,95 @@ class PacketHandler(ABC):
         self.window_size = window_size
 
 
-        def send_data(self, data: bytes) -> None:       # takes a byte object, turns it into 1008 byte pieces, turns those into segments, sends them
-            pkt_queue = queue.Queue()                   # queue with PAYLOAD_SIZE bytes, except for the last one; possible less than PAYLOAD bytes.
-            while len(data) > 0:
-                if len(data) >= PAYLOAD_SIZE:
-                    pkt_queue.put(data[:PAYLOAD_SIZE])
-                    data = data[PAYLOAD_SIZE:]
-                else:
-                    pkt_queue.put(data)
-            
-            for seg in self.build_seg_queue(self, list(pkt_queue)):
-                self.seg_queue.put(seg)
-            self.send_window_seqments(self) 
-
-            return None
-
-
-        def handle_rcvd_seg(self, seq_field: bytes, ack_field: bytes, ACK, payload: bytes) -> bytes: # handle incoming traffic; differentiate between a packet with the ACK set, and a data packet. 
-            """ 
-            A segment is recieved by a socket and unpacked. The payload and part of the unpacked header
-            is given as input. This is handled by the specific instance of the handler. This function returns
-            the data recieved in correct order. If a call contains not in-order data the function will return 
-            an empty bytes object and depending on the specific handler it might buffer or discard the data recieved.
-            """ 
-            if ACK:
-                return self.handle_ack(self, seq_field, ack_field, payload)
-            return self.handle_data(self, seq_field, ack_field, payload)
-
-
-        @abstractmethod
-        def build_seg_queue(self, pkt_list: list[bytes]) -> queue.Queue[bytes]:
-            """
-            This function constructs a queue of segments. These segments are constructed by the 
-            specific handler and based on the data. 
-            """
-            pass
-
-        @abstractmethod
-        def send_window_segments(self) -> None:
-            """
-            This functions sends all segments within the window which the specific handler decides
-            to send. This function needs to be called every time the send_base is updated and when
-            the specifc handler needs to send all segments in the window.
-            """
-            pass 
-
-        @abstractmethod
-        def handle_ack(self, ack_field: bytes):
-            """
-            This function handles incoming messages with an ACK flag. It checks if the ACK is in order.
-            If it is the send_base, and the ack_queue is updated in handle_ack_queue.
-            If it is not in order this is handled based on the sprecific handler implementation.
-            """
-            pass 
-
-
-        @abstractmethod
-        def handle_data(self, seq_field: int):
-            '''
-            This function handles incomming messages without an ACK flag, and thus this is a segment with data.
-            If the data segment is in-order then this segment or possibly a buffer of data (depending on the specific handler) 
-            is sent back, the socket may assume all data recieved from the packet_handler is in order.
-            If the data segment in out of order the way to handle is handler specific.
-            There is also an ACK send, which again is based on the specific handler.
-            '''
-            pass 
-
-        @abstractmethod
-        def build_ack_queue(self) -> None:  
-            ''' 
-            the purpose of the ack queue is holding the acknumbers of the to be expected acks
-            if the received ack is not equal to the first element in the queue with acks, the packets are out of order.
-            Though, this function just builds the ack queue.
-            '''
-            pass
-
-        @abstractmethod
-        def update_ack_queue(self, seq_num: int) -> None:
-            '''
-            This function is called when new segments are to be pushed onto the seg_queue
-            It takes an integer and pushes it onto the ack_queue
-            '''
-            pass
-
+    def send_data(self, data: bytes) -> None:       # takes a byte object, turns it into 1008 byte pieces, turns those into segments, sends them
+        pkt_queue = queue.Queue()                   # queue with PAYLOAD_SIZE bytes, except for the last one; possible less than PAYLOAD bytes.
+        while len(data) > 0:
+            if len(data) >= PAYLOAD_SIZE:
+                pkt_queue.put(data[:PAYLOAD_SIZE])
+                data = data[PAYLOAD_SIZE:]
+            else:
+                pkt_queue.put(data)
         
-        @abstractmethod
-        def acknowledge_number(self, seq_num: int) -> None:
-            '''
-            This function acknowledges the packet 'segment',
-            so it does some operations on the ack_queue, depending
-            on which protocol (GBN, SR, TCP) is used.
-            '''
-            pass
+        for seg in self.build_seg_queue(self, list(pkt_queue)):
+            self.seg_queue.put(seg)
+        self.send_window_seqments(self) 
+
+        return None
+
+
+    def handle_rcvd_seg(self, seq_field: bytes, ack_field: bytes, ACK, payload: bytes) -> bytes: # handle incoming traffic; differentiate between a packet with the ACK set, and a data packet. 
+        """ 
+        A segment is recieved by a socket and unpacked. The payload and part of the unpacked header
+        is given as input. This is handled by the specific instance of the handler. This function returns
+        the data recieved in correct order. If a call contains not in-order data the function will return 
+        an empty bytes object and depending on the specific handler it might buffer or discard the data recieved.
+        """ 
+        if ACK:
+            return self.handle_ack(self, seq_field, ack_field, payload)
+        return self.handle_data(self, seq_field, ack_field, payload)
+
+
+    @abstractmethod
+    def build_seg_queue(self, pkt_list: list[bytes]) -> queue.Queue[bytes]:
+        """
+        This function constructs a queue of segments. These segments are constructed by the 
+        specific handler and based on the data. 
+        """
+        pass
+
+    @abstractmethod
+    def send_window_segments(self) -> None:
+        """
+        This functions sends all segments within the window which the specific handler decides
+        to send. This function needs to be called every time the send_base is updated and when
+        the specifc handler needs to send all segments in the window.
+        """
+        pass 
+
+    @abstractmethod
+    def handle_ack(self, ack_field: bytes):
+        """
+        This function handles incoming messages with an ACK flag. It checks if the ACK is in order.
+        If it is the send_base, and the ack_queue is updated in handle_ack_queue.
+        If it is not in order this is handled based on the sprecific handler implementation.
+        """
+        pass 
+
+
+    @abstractmethod
+    def handle_data(self, seq_field: int):
+        '''
+        This function handles incomming messages without an ACK flag, and thus this is a segment with data.
+        If the data segment is in-order then this segment or possibly a buffer of data (depending on the specific handler) 
+        is sent back, the socket may assume all data recieved from the packet_handler is in order.
+        If the data segment in out of order the way to handle is handler specific.
+        There is also an ACK send, which again is based on the specific handler.
+        '''
+        pass 
+
+    @abstractmethod
+    def build_ack_queue(self) -> None:  
+        ''' 
+        the purpose of the ack queue is holding the acknumbers of the to be expected acks
+        if the received ack is not equal to the first element in the queue with acks, the packets are out of order.
+        Though, this function just builds the ack queue.
+        '''
+        pass
+
+    @abstractmethod
+    def update_ack_queue(self, seq_num: int) -> None:
+        '''
+        This function is called when new segments are to be pushed onto the seg_queue
+        It takes an integer and pushes it onto the ack_queue
+        '''
+        pass
+
+    
+    @abstractmethod
+    def acknowledge_number(self, seq_num: int) -> None:
+        '''
+        This function acknowledges the packet 'segment',
+        so it does some operations on the ack_queue, depending
+        on which protocol (GBN, SR, TCP) is used.
+        '''
+        pass
