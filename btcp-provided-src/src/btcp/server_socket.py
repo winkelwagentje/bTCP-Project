3538a92ba921e5eada98141e2889848b332a3d15
sevy <jui.yvesvanhaaren@gmail.com>
2024-03-19 14:56:13 +0100

    klets klets

--- btcp-provided-src/src/btcp/server_socket.py
+++ btcp-provided-src/src/btcp/server_socket.py
@@ -275,7 +275,7 @@ class BTCPServerSocket(BTCPSocket):
         elif flags == 0: # in syn rcvd, so not yet established, but we are already recvng data
             pseudo_header = BTCPSocket.build_segment_header(seqnum=self._ISN, acknum=seq_num+1, syn_set=True, ack_set=True, window=self._window)
             header = BTCPSocket.build_segment_header(seqnum=self._ISN, acknum=seq_num+1, syn_set=True, ack_set=True, window=self._window, checksum=BTCPSocket.in_cksum(pseudo_header))
-            self._lossy_layer.send_segment(segment)
+            self._lossy_layer.send_segment(header + bytes(PAYLOAD_SIZE))
 
     def _established_segment_received(self, segment):
 
